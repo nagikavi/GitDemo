81cd8a65c751fa11e9803aa1a3b5655a25bc6fc2
kavitha <kavi_puppy@yahoo.com>
2019-05-19 14:53:03 -0700

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void nativeAppandroid()
+	{
+		System.out.println("nativeAppandroid");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserAutomate()
+	{
+		System.out.println("browserAutomate");
+	}
+	
+	@Test
+	public void elementUi()
+	{
+		System.out.println("elementUi");
+	}
+
+}
